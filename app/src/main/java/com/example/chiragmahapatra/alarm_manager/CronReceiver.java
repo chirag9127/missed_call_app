8fac1946273b1c588109300aae6baf9386838b5e
Chirag Mahapatra <chirag.mahapatra@gmail.com>
2016-11-06 17:57:37 -0800

    first commit

---
+++ app/src/main/java/com/example/chiragmahapatra/alarm_manager/CronReceiver.java
@@ -0,0 +1,21 @@
+package com.example.chiragmahapatra.alarm_manager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by chiragmahapatra on 8/27/16.
+ */
+public class CronReceiver extends BroadcastReceiver {
+    public static final int REQUEST_CODE = 12345;
+
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        Intent i = new Intent(context, AlarmService.class);
+        context.startService(i);
+
+    }
+}
